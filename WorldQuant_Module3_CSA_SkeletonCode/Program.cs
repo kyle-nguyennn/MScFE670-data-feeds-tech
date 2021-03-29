560f866d16cfbfad2f1c8035be8e71dbf3d7ec0e
Unknown <nghia002@outlook.com>
2021-03-29 14:01:55 +0800

    implement AddPropertyToWorksheet

--- WorldQuant_Module3_CSA_SkeletonCode/Program.cs
+++ WorldQuant_Module3_CSA_SkeletonCode/Program.cs
@@ -5,8 +5,11 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
 {
     class Program
     {
+        static Excel.Worksheet worksheet;
         static Excel.Workbook workbook;
         static Excel.Application app;
+        static string[] headers = { "Size", "Suburb", "City", "Market value" };
+        static int nRows = 0;
         static string filepath = Environment.CurrentDirectory + @"\property_pricing.xlsx";
 
         static void Main(string[] args)
@@ -21,6 +24,21 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
             {
                 SetUp();
             }
+            worksheet = workbook.Worksheets.get_Item(1);
+            // count the number of rows
+            string tempText;
+            do
+            {
+                nRows++;
+                tempText = worksheet.Cells[nRows, 1].Text as string;
+            } while (!string.IsNullOrEmpty(tempText));
+            if (nRows == 1) // set headers
+            {
+                for (int i = 0; i < headers.Length; i++) worksheet.Cells[1, i + 1] = headers[i];
+                nRows++;
+            }
+            workbook.Save();
+            
 
             var input = "";
             while (input != "x")
@@ -91,12 +109,18 @@ namespace WorldQuant_Module3_CSA_SkeletonCode
         static void SetUp()
         {
             workbook = app.Workbooks.Add();
+            // set up headers
             workbook.SaveAs(filepath);
         }
 
         static void AddPropertyToWorksheet(float size, string suburb, string city, float value)
         {
-            // TODO: Implement this method
+            worksheet.Cells[nRows, 1] = size;
+            worksheet.Cells[nRows, 2] = suburb;
+            worksheet.Cells[nRows, 3] = city;
+            worksheet.Cells[nRows, 4] = value;
+            nRows++;
+            workbook.Save();
         }
 
         static float CalculateMean()
